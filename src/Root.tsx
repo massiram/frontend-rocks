12106faf71b4f8883915928c6f51be2198cebe08
massiram <m.rambaldini09@gmail.com>
2026-02-26 14:13:25 +0000

    Modifica Root e card

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,14 +4,6 @@ import { PokeAPI } from "./api";
 */
 
 interface Props {
-  id: number;
-  image: string;
-  name: string;
-  types: string[];
-}
-
-type Props = {
-  id: number;
   image: string;
   name: string;
   types: string[];
@@ -29,36 +21,11 @@ function Card(props: Props) {
 
 export function Root() {
   return <Card
-    id={0}
     image="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png"
     name="Pikachu - 0"
     types={["electric"]}
     />
 }
-function getTypeColor(type: string): string {
-  return typeColors[type];
-}
-
-const typeColors: { [key: string]: string } = {
-  fire: "bg-red-500",
-  water: "bg-blue-500",
-  grass: "bg-green-500",
-  electric: "bg-yellow-400",
-  psychic: "bg-pink-500",
-  ice: "bg-cyan-400",
-  dragon: "bg-purple-700",
-  dark: "bg-gray-700",
-  fairy: "bg-pink-300",
-  normal: "bg-gray-400",
-  fighting: "bg-red-700",
-  flying: "bg-indigo-400",
-  poison: "bg-purple-500",
-  ground: "bg-yellow-600",
-  rock: "bg-yellow-800",
-  bug: "bg-green-700",
-  ghost: "bg-indigo-700",
-  steel: "bg-gray-500",
-};
 
 /*
 interface PokemonCard {
